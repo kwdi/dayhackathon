5802f7861821e4ad873733e83de9e96d82858acd
kwdi <kostas.droid@gmail.com>
2021-07-01 11:20:46 +0200

    basic api working, create read post

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
+const { DateTime } = require("luxon");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +10,56 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
+const usersDB = {  end: { pass: '123' }, user: { pass: '123' }  };
+const usersMsg = { 
+  end: [{from: 'kostas' , msg: 'hello friend', date : '1999'}, 
+  {from: 'kostas' , msg: 'bye friend', date : '2000'}] ,
+  user: []
+};
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+app.post('/api/adduser', (req, res) => {
+  if(!usersDB.hasOwnProperty(req.body.name)){
+    usersDB[`${req.body.name}`]= {pass: req.body.pass}
+    usersMsg[`${req.body.name}`] = [];
+    return res.json({ success: `hello ${req.body.name}`});
+  }
+  return res.json({ error: "error user exists"});
+});
+
+app.get('/api/readmail', (req, res) => {
+  const name = req.headers.name;
+  const pass = req.headers.pass;
+  if(usersDB.hasOwnProperty(name) && usersDB[`${name}`].pass === pass){
+    return res.json(usersMsg[`${name}`])
+  }
+    return res.json({ error: "error wrong name or pass"});
+})
+
+app.post('/api/sendmail', (req, res) => {
+  const name = req.headers.name;
+  const pass = req.headers.pass;
+  const receiver = req.body.receiver;
+  const msg = req.body.msg;
+  if(usersDB.hasOwnProperty(name) && usersDB[`${name}`].pass === pass){
+    const dateF = DateTime.now().toFormat('f');;;
+    if(usersDB.hasOwnProperty(receiver)){
+      usersMsg[`${req.body.receiver}`].push({from: name, msg: msg, date: dateF})
+      return res.json({ success: 'email send'})
+    }
+
+  }
+    return res.json({ error: "error no such user"});
+
+})
+
+app.get('/', (req, res) => {
+  console.log(req.query)
+})
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
