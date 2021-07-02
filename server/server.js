3f0946102ae321f85ed413b65f95411bbbca6672
kwdi <kostas.droid@gmail.com>
2021-07-02 09:57:56 +0200

    linted server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
-const { DateTime } = require("luxon");
+const cors = require('cors');
+const { DateTime } = require('luxon');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,81 +10,53 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-
-const usersDB = {  end: { pass: '123' }, user: { pass: '123' }  };
-const usersMsg = { 
-  end: [{from: 'kostas' , msg: 'hello friend', date : '1999'}, 
-  {from: 'kostas' , msg: 'bye friend', date : '2000'}] ,
-  user: []
+const usersDB = { end: { pass: '123' }, user: { pass: '123' } };
+const usersMsg = {
+  end: [{ from: 'kostas', msg: 'hello friend', date: '1999' },
+    { from: 'kostas', msg: 'bye friend', date: '2000' }],
+  user: [],
 };
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
-// app.post('/api/login', (req, res) => { 
-//   console.log("hello from login")
-//   console.log(req.body)
-//   const name = req.body.name;
-//   const pass = req.body.pass;
-//   if(usersDB.hasOwnProperty(name) && usersDB[`${name}`].pass === pass){
-//     return res.json({ success: `hello ${name}`});
-//   }
-//   return res.json({ error: "Wrong user or password"});
-// });
-
 app.post('/api/adduser', (req, res) => {
-  console.log("hello from adduser")
-  const name = req.body.name;
-  const pass = req.body.pass;
-  if(!usersDB.hasOwnProperty(name)){
-    usersDB[`${name}`]= {pass: pass}
+  const { name } = req.body;
+  const { pass } = req.body;
+  if (!Object.prototype.hasOwnProperty.call(usersDB, name)) {
+    usersDB[`${name}`] = { pass };
     usersMsg[`${name}`] = [];
-    return res.json({ success: `hello ${name}`});
+    return res.json({ success: `hello ${name}` });
   }
-  if(usersDB.hasOwnProperty(name) && usersDB[`${name}`].pass === pass){
-    return res.json({ success: `hello ${name}`});
+  if (Object.prototype.hasOwnProperty.call(usersDB, name) && usersDB[`${name}`].pass === pass) {
+    return res.json({ success: `hello ${name}` });
   }
-  return res.json({ error: "error wrong pass"});
+  return res.json({ error: 'error wrong pass' });
 });
 
 app.get('/api/readmail', (req, res) => {
-  console.log("hello from readmail")
-  const name = req.headers.name;
-  const pass = req.headers.pass;
-  if(usersDB.hasOwnProperty(name) && usersDB[`${name}`].pass === pass){
-    return res.json(usersMsg[`${name}`])
+  const { name } = req.headers;
+  const { pass } = req.headers;
+  if (Object.prototype.hasOwnProperty.call(usersDB, name) && usersDB[`${name}`].pass === pass) {
+    return res.json(usersMsg[`${name}`]);
   }
-    return res.json({ error: "error wrong name or pass"});
-})
-
-app.post('/api/sendmail', (req, res) => {
-  console.log("hello from sendmail")
-  const name = req.headers.name;
-  const pass = req.headers.pass;
-  const receiver = req.body.receiver;
-  const msg = req.body.msg;
-  if(usersDB.hasOwnProperty(name) && usersDB[`${name}`].pass === pass){
-    const dateF = DateTime.now().toFormat('f');;;
-    if(usersDB.hasOwnProperty(receiver)){
-      usersMsg[`${req.body.receiver}`].push({from: name, msg: msg, date: dateF})
-      return res.json({ success: 'email send'})
-    }
-
-  }
-    return res.json({ error: "error no such user"});
-
-})
-
-app.get('/', (req, res) => {
-  console.log(req.query)
-})
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+  return res.json({ error: 'error wrong name or pass' });
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.post('/api/sendmail', (req, res) => {
+  const { name } = req.headers;
+  const { pass } = req.headers;
+  const { receiver } = req.body;
+  const { msg } = req.body;
+  if (Object.prototype.hasOwnProperty.call(usersDB, name) && usersDB[`${name}`].pass === pass) {
+    const dateF = DateTime.now().toFormat('f');
+    if (Object.prototype.hasOwnProperty.call(usersDB, receiver)) {
+      usersMsg[`${req.body.receiver}`].push({ from: name, msg, date: dateF });
+      return res.json({ success: 'email send' });
+    }
+  }
+  return res.json({ error: 'error no such user' });
+});
+
+app.listen(port);
